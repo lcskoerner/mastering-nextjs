921454542d175440b2b08e96c8c6a35fde1d7a0f
Lucas Koerner <lcskoerner@gmail.com>
2020-11-18 21:09:10 -0500

    query github

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,6 +1,6 @@
 import Header from '../../../components/header'
 
-const Post = ({ title, body}) => {
+const Post = ({ title, body }) => {
   return (
     <>
       <Header />
@@ -11,9 +11,9 @@ const Post = ({ title, body}) => {
 }
 
 Post.getInitialProps = async ({ query }) => {
-  const url = `https://jsonplaceholder.typicode.com/posts/${query.id}`;
-  const post = fetch(url).then(res => res.json());
-  return post;
+  const url = `https://jsonplaceholder.typicode.com/posts/${query.id}`
+  const post = fetch(url).then(res => res.json())
+  return post
 }
 
-export default Post;
+export default Post
